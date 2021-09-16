30ca46ca73293ef73069129507de5acdba7decbb
tbctk <60549285+tbctk@users.noreply.github.com>
2021-09-16 14:52:57 -0400

    updated p1.py

--- problem_sets/ps1/p1.py
+++ problem_sets/ps1/p1.py
@@ -13,6 +13,16 @@ def deriv(fun,x,dx):
 
 opt = eps**(1/5)
 
+print('For f(x) = exp(x), x = 42:')
 print('Relative error with optimal dx: ',(deriv(np.exp,x,opt)-np.exp(x))/np.exp(x))
 print('Relative error with 10 times larger dx: ',(deriv(np.exp,x,opt*10)-np.exp(x))/np.exp(x))
 print('Relative error with 10 times smaller dx: ',(deriv(np.exp,x,opt/10)-np.exp(x))/np.exp(x))
+
+def fun(x):
+    return np.exp(0.01*x)
+
+print('\nFor f(x) = exp(0.01x), x = 42:')
+print('Relative error with optimal dx: ',(deriv(fun,x,opt)-0.01*fun(x))/(0.01*fun(x)))
+print('Relative error with 10 times larger dx: ',(deriv(fun,x,opt*10)-0.01*fun(x))/(0.01*fun(x)))
+print('Relative error with 10 times smaller dx: ',(deriv(fun,x,opt/10)-0.01*fun(x))/(0.01*fun(x)))
+
