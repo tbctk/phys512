82e9312f609b35327e37a73ddb28cdf35f6e0d2a
tbctk <60549285+tbctk@users.noreply.github.com>
2021-09-16 15:17:29 -0400

    updated problem 2 p2.py

--- problem_sets/ps1/p2.py
+++ problem_sets/ps1/p2.py
@@ -1,18 +1,29 @@
 import numpy as np
 
 def ndiff(fun,x,full=False):
-    eps = np.finfo(float).eps
+    # Use numpy arrays to make element-wise arithmetic easy
+    #x = np.array(x)
+    
+    # Calculate optimal dx
+    eps = 2**(-52)
     dx = eps**(1/3)
+    dx = (x+dx)-x
+    
+    # Evaluate derivative
     f1 = fun(x+dx)
     f2 = fun(x-dx)
     deriv = (f1-f2)/(2*dx)
+    
     if full:
+        # Leading order error is (f'''(x)/3)dx^2. We drop constant terms to get an approximate error dx^2.
         err = dx**2
         return deriv,dx,err
     else:
         return deriv
 
-x = 1
-diff = ndiff(np.exp,x,True)
+x = 42
+diff = ndiff(np.exp,x,False)
 
-print('Expected: ',np.exp(x),' Numerical: ',diff)
+print('Output for f=exp, x=42, full=True: ',ndiff(np.exp,x,True))
+print('Output for f=exp, x=42, full=False: ',diff)
+print('Relative Error: ',(np.exp(x)-diff)/np.exp(x))
