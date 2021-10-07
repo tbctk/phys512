824abbc71e389c779c4c6f54233f2b7cb8392c2a
tbctk <60549285+tbctk@users.noreply.github.com>
2021-10-07 19:48:27 -0400

    changed to p3

--- problem_sets/ps3/p3.py
+++ problem_sets/ps3/p3.py
@@ -30,7 +30,23 @@ lhs = A.T@A
 rhs = A.T@z
 fitp = np.linalg.inv(lhs)@rhs
 
-realp = get_pars(fitp)
-
+p = get_pars(fitp)
 print("Modfied parameters: a=",fitp[0],", b=",fitp[1],", c=",fitp[2],", d=",fitp[3])
-print("Original parameters: a=",realp[0]," x_0=",realp[1],", y_0=",realp[2],", z_0=",realp[3])
+print("Original parameters: a=",p[0]," x_0=",p[1],", y_0=",p[2],", z_0=",p[3])
+
+# Now find noise estimate based on this model
+
+z_fit = paraboloid(fitp,x,y)
+noise = np.std(z_fit-z)
+N = np.eye(len(z))*noise**2
+Ninv = np.eye(len(z))*noise**-2
+tmp = np.linalg.inv(A.T@Ninv@A)
+p_errs = np.sqrt(np.diag(tmp))
+print("Error on a: ",p_errs[0])
+
+# Get error in f
+dfda = (4*fitp[0]**2)**-1
+f_err = dfda*p_errs[0]
+print("Focal length: ",(4*fitp[0])**-1,"Error on focal length: ",f_err)
+
+
