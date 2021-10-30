749e46cbaa9872953d62ebf7f0c88aca3aad0a68
tbctk <60549285+tbctk@users.noreply.github.com>
2021-10-29 20:26:27 -0400

    renamed some things

--- problem_sets/ps5/p1234.py
+++ problem_sets/ps5/p1234.py
@@ -32,8 +32,21 @@ def self_corr(arr,shift=0):
     arr2 = shift_array(arr,shift)
     return correlation(arr,arr2)
 
+## PROBLEM 4
+def conv_safe(arr1,arr2):
+    n1 = len(arr1)
+    n2 = len(arr2)
+    n_safe = n1+n2
+    arr1_safe = np.zeros(n_safe)
+    arr2_safe = np.zeros(n_safe)
+    arr1_safe = np.concatenate((np.zeros(n2),arr1))
+    arr2_safe = np.concatenate((np.zeros(n1),arr2))
+    return convolve(arr1_safe,arr2_safe)
+
+
+
 ## EXECUTION
-savefigs = True
+savefigs = False
 
 def setup():
     pars = [0,1,50,10]
@@ -66,4 +79,17 @@ def main3():
     plt.legend(loc="upper right",title="Shift")
     plt.savefig("gauss_correlation_shift.png") if savefigs else None
     plt.show()
+
+def main4():
+    pars = [0,1,50,10]
+    x = np.arange(100)
+    y = gauss(pars,x)
+    pars2 = [0,1,20,4]
+    x2 = np.arange(60)
+    y2 = gauss(pars2,x2)
+    conv = conv_safe(y,y2)
+    plt.figure()
+    plt.plot(conv)
+    plt.savefig("safe_convolution.png") if savefigs else None
+    print(len(conv))
 
--- problem_sets/ps5/p5.py
+++ problem_sets/ps5/p5.py
@@ -1,1 +1,10 @@
 import numpy as np
+
+## PROBLEM 5
+
+def anal_sin_dft(k_0,N,k):
+    top1 = 1-np.exp(-2j*np.pi*(k-k_0))
+    bot1 = 1-np.exp(-2j*np.pi*(k-k_0)/N)
+    top2 = 1-np.exp(-2j*np.pi*(k+k_0))
+    bot2 = 1-np.exp(-2j*np.pi*(k+k_0)/N)
+    return (top1/bot1 - top2/bot2)/2j
